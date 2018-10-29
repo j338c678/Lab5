78908130f604fd975603c97a5a54bffc8ecfa32f
Junpeng Cao <j338c678@gmail.com>
2018-10-28 19:37:10 -0500

    final commit

---
+++ ViewUserPosts.php
@@ -0,0 +1,31 @@
+<?php
+  echo "<html><body><table>";
+  $select = $_POST["n"];
+  $mysqli = new mysqli("mysql.eecs.ku.edu","j338c678","moh3thoh","j338c678");
+  if($mysqli->connect_errno)
+  {
+    printf("Connection Failed! %s\n",$mysqli->connect_error);
+    exit();
+  }
+    $query = "SELECT content FROM Posts WHERE author_id = '$select'";
+    if($result=$mysqli->query($query))
+    {
+      if(mysqli_num_rows($result)==0)
+      {
+        echo"empty post!";
+      }
+      else
+      {
+        while($row=$result->fetch_assoc())
+        {
+            echo "<tr><td>".$row["content"]."</td></tr>";
+        }
+      }
+    }
+    else
+    {
+      echo "<tr><td>Nothing to post</td></tr>";
+    }
+    $mysqli->close();
+    echo "</table></body></html>";
+?>
